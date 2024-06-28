494fe50c2e144a442ec1fb416275c0e95b0ddb42
xhaqur xhariph <shakuritoshariff@gmail.com>
2024-06-28 18:34:42 +0300

    adds index.js

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// code your solution here
+function saturdayFun(hobby = "roller-skate") {
+    return `This Saturday, I want to ${hobby}!`;
+}
+console.log(saturdayFun());
+console.log(saturdayFun("bathe my dog"));
+
+const mondayWork = function (activity = "go to the office"){
+    return `This Monday, I will ${activity}.`;
+}
+console.log(mondayWork());
+console.log(mondayWork("work from home"));
+
+function wrapAdjective(wrap = "*"){
+    return function(adjective){
+        return `You are ${wrap}${adjective}${wrap}!`;
+    };
+}
+wrapAdjective()("a hard worker");
+wrapAdjective("!!!")("a dedicated programmer");
